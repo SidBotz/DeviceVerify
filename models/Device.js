42d78e8eb801c894f77e90a169f98792bd166581
SidBotz <sidop148@gmail.com>
2024-12-21 10:41:33 +0530

    Delete models directory

--- models/Device.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const DeviceSchema = new mongoose.Schema({
-    deviceId: { type: String, required: true, unique: true },
-    userId: { type: String, required: true },
-});
-
-module.exports = mongoose.models.Device || mongoose.model('Device', DeviceSchema);
