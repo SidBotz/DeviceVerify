5bc38a23b66faff72a55f7e41f7f82f8c943ffd8
SidBotz <sidop148@gmail.com>
2024-12-21 10:02:51 +0530

    Create db.js

---
+++ utils/db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectToDatabase() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }).then((mongoose) => mongoose);
+    }
+
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
+
+module.exports = connectToDatabase;
